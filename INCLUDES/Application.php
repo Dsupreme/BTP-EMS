6137de4bea0b357bf357e9250cddef480d776db8
Manmeet Singh Oberoi <ms_oberoi@outlook.com>
2014-02-16 00:47:21 +0530

    Added new folder to hold .php pages and added new variables file

--- INCLUDES/Application.php
+++ INCLUDES/Application.php
@@ -1,18 +1,29 @@
+<?php 
+	include 'variables.php';
+
+?>
+
+
+
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
 
-<title>Untitled Document</title>
+<title>Application Form</title>
 	<!-- CSS Links -->
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.css" media="screen" />
-    <link rel="stylesheet" type="text/css" href="css/application.css" media="screen" />
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="../css/application.css" media="screen" />
     
     
     <!--Javascript Links-->
-    <script type="text/javascript" src="js/smoothscroll.js"></script><!--Smooth Scroll Animation Javascript-->
+    <script type="text/javascript" src="../js/smoothscroll.js"></script><!--Smooth Scroll Animation Javascript-->
 </head>
+
+
+
 
 <body  style="background-color:#09F">
 <form method="post">
---
+++ INCLUDES/Variables.php
@@ -0,0 +1,38 @@
+<?php 
+	
+	if(isset($_REQUEST['candidate_fname']))
+	{
+		$candidate_fname=$_REQUEST['candidate_fname'];
+	}
+	
+	if(isset($_REQUEST['candidate_mname']))
+	{
+		$candiddate_mname=$_REQUEST['candidate_mname'];
+	}
+	
+	if(isset($_REQUEST['candidate_lname']))
+	{
+		$candidate_lname=$_REQUEST['candidate_lname'];
+	}
+	
+	if(isset($_REQUEST['f_candidate']))
+	{
+		$f_candidate=$_REQUEST['f_candidate'];
+	}
+	
+	
+	
+
+?>
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+</body>
+</html>
