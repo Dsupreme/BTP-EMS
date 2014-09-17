6d0eadace7662d6c2ca6e042e9457155d57cdfbd
naresh11067 <naresh11067@iiitd.ac.in>
2014-09-17 15:28:47 +0530

    Half of the work done !
    As asked, syncing! will be doing it complete soon!

--- INCLUDES/Application.php
+++ INCLUDES/Application.php
@@ -31,41 +31,90 @@
 	
 <?php
 	if(isset($_POST['submitbtn'])) { 
-		mysql_query("insert into pdetails (flag, firstname, middlename, lastname, fathername, mothername, DOB, nationality, sex, c_addr, c_city, c_state, c_pin, c_phone, c_mobile, p_addr, p_city, p_state, p_pin, p_phone, p_mobile) values (1,'$candidate_fname', '$candidate_mname','$candidate_lname', '$f_candidate','$m_candidate', '$dob_candidate', '$nationality_candidate', '$gender', '$c_address', '$c_city', '$c_state', '$c_pin', '$c_landline', '$c_mobile', '$p_address', '$p_city' , '$p_state', '$p_pin', '$p_landline', '$p_mobile')
-					") or die(mysql_error());
-		$result = mysql_query("SELECT * FROM pdetails WHERE flag=1;") ;
-		while($row = mysql_fetch_array($result)) { $apid = $row['app_ID'];}
-		$countd  = 0;
-		foreach($c_degree as $h) { $countd=$countd+1;}
-		$counte = 0;
-		foreach($c_salary as $i)
-		{
-		$counte=$counte+1;
+		$signup_error = "";
+		if ($_SERVER["REQUEST_METHOD"] == "POST") {
+			if(empty($candidate_fname)and(empty($candidate_lname))){
+				$signup_error.=" First name and last name , both are required. ";
+			}
+			else {
+				if (!preg_match('/^[a-zA-Z]+$/', $candidate_fname)) {
+					$signup_error.="Invalid Firstname. Remove invalid characters";
+				}
+				else if (!preg_match('/^[a-zA-Z]+$/', $candidate_lname)) {
+					$signup_error.="Invalid Lastname. Remove invalid characters";
+				}
+			}
+			if(empty($f_candidate)){$signup_error.=" Fathers name is required. ";}else{if (!preg_match('/^[a-zA-Z]+$/', $f_candidate)) {
+					$signup_error.="Invalid Fathers name. Remove invalid characters";
+				}}
+			if(empty($m_candidate)){$signup_error.=" Mothers name is required. ";}else{if (!preg_match('/^[a-zA-Z]+$/', $m_candidate)) {
+					$signup_error.="Invalid Mothers name. Remove invalid characters";
+				}}
+			if(empty($nationality_candidate)){$signup_error.=" Nationality is required. ";}else{if (!preg_match('/^[a-zA-Z]+$/', $nationality_candidate)) {
+					$signup_error.="Invalid Nationality. Remove invalid characters, only alphabets allowed";
+				}}
+			if(empty($dob_candidate)){$signup_error.=" Date of Birth is required. ";}else{if(!preg_match('/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/', $dob_candidate)){ 
+					$signup_error.="Invalid Date format , Please fill as suggested in tool-tip";					
+				}}
+			if(empty($gender)){$signup_error.=" Gender is not selected. ";}
+			
+
+			
+			
 		}
 		
-		$expe = array($c_from,$c_to,$c_desg,$c_org,$c_resp,$c_salary);	
-		for($u = 0 ; $u < $counte ; $u++){
-		$from = $expe[0][$u];
-		$to = $expe[1][$u];
-		$desg = $expe[2][$u];
-		$org = $expe[3][$u];
-		$resp = $expe[4][$u];
-		$salary = $expe[5][$u];
-		mysql_query("insert into experience (app_ID, per_from, per_to, organization, designation,responsibility,salary) values ('$apid','$from','$to','$desg','$org','$resp','$salary');
-					") or die(mysql_error());	
+		
+		
+		
+
+		
+		
+		if (strcmp($signup_error,"")!='0') {
+			echo "<script language='javascript' type='text/javascript'>";
+			echo "alert('$signup_error');";
+			echo "</script>";
 		}
-		$qual = array($c_degree,$c_specialization,$c_university,$c_year,$c_marks);
-		for($v = 0 ; $v < $countd ; $v++){
-		$deg = $qual[0][$v];
-		$spec = $qual[1][$v];
-		$board = $qual[2][$v];
-		$yo = $qual[3][$v];
-		$mar = $qual[4][$v];
-		mysql_query("insert into edu_qual (app_ID, degree, specialization, boarduniv, yoc, marks) values ('$apid','$deg','$spec','$board','$yo','$mar');
-					") or die(mysql_error());
+		else{
+			mysql_query("insert into pdetails (flag, firstname, middlename, lastname, fathername, mothername, DOB, nationality, sex, c_addr, c_city, c_state, c_pin, c_phone, c_mobile, p_addr, p_city, p_state, p_pin, p_phone, p_mobile) values (1,'$candidate_fname', '$candidate_mname','$candidate_lname', '$f_candidate','$m_candidate', '$dob_candidate', '$nationality_candidate', '$gender', '$c_address', '$c_city', '$c_state', '$c_pin', '$c_landline', '$c_mobile', '$p_address', '$p_city' , '$p_state', '$p_pin', '$p_landline', '$p_mobile')
+						") or die(mysql_error());
+			$result = mysql_query("SELECT * FROM pdetails WHERE flag=1;") ;
+			while($row = mysql_fetch_array($result)) { $apid = $row['app_ID'];}
+			$countd  = 0;
+			foreach($c_degree as $h) { $countd=$countd+1;}
+			$counte = 0;
+			foreach($c_salary as $i)
+			{
+			$counte=$counte+1;
+			}
+			
+			$expe = array($c_from,$c_to,$c_desg,$c_org,$c_resp,$c_salary);	
+			for($u = 0 ; $u < $counte ; $u++){
+			$from = $expe[0][$u];
+			$to = $expe[1][$u];
+			$desg = $expe[2][$u];
+			$org = $expe[3][$u];
+			$resp = $expe[4][$u];
+			$salary = $expe[5][$u];
+			mysql_query("insert into experience (app_ID, per_from, per_to, organization, designation,responsibility,salary) values ('$apid','$from','$to','$desg','$org','$resp','$salary');
+						") or die(mysql_error());	
+			}
+			$qual = array($c_degree,$c_specialization,$c_university,$c_year,$c_marks);
+			for($v = 0 ; $v < $countd ; $v++){
+			$deg = $qual[0][$v];
+			$spec = $qual[1][$v];
+			$board = $qual[2][$v];
+			$yo = $qual[3][$v];
+			$mar = $qual[4][$v];
+			mysql_query("insert into edu_qual (app_ID, degree, specialization, boarduniv, yoc, marks) values ('$apid','$deg','$spec','$board','$yo','$mar');
+						") or die(mysql_error());
+			}
+			mysql_query("UPDATE pdetails SET flag=0 WHERE flag = 1;");
+			echo "<script language='javascript' type='text/javascript'>";
+			echo "alert('The data is successfully entered in the database');";
+			echo "</script>";
 		}
-		mysql_query("UPDATE pdetails SET flag=0 WHERE flag = 1;");		
 	}
+	
 	
 	
 	/*mysql_query("INSERT INTO pdetails (, )  )") or die(mysql_error());*/
@@ -129,7 +178,7 @@
                         	<td ><input type="text" name="candidate_fname" placeholder="Mandatory"/></td>
                         	<td class="Label" >Middle Name</td>
                             <td id="colon">:</td>
-                        	<td ><input type="text" name="candidate_mname"  placeholder="Mandatory" /></td>
+                        	<td ><input type="text" name="candidate_mname" /></td>
                         	<td class="Label" >Last Name </td>
                             <td id="colon">:</td>
                         	<td ><input type="text" name="candidate_lname" placeholder="Mandatory" /></td>
