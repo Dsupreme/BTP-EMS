f51848b492c674b0f6ddbdc937c185f9b5de9562
Manmeet Singh Oberoi <ms_oberoi@outlook.com>
2014-11-10 19:42:58 +0530

    minor changes.
    Accordion bekaar hai for us

--- INCLUDES/Leave.php
+++ INCLUDES/Leave.php
@@ -117,57 +117,19 @@
             </section> 
         </div>        
 		<div class="container">
-
             <div class="panel panel-default" id="leave_history">
     		    <div class="panel-heading">Leave History</div>
                 <div class="panel-body">
+                    <ul class="list-group">
+                        <li class="list-group-item">Cras justo odio</li>
+                        <li class="list-group-item">Dapibus ac facilisis in</li>
+                        <li class="list-group-item">Morbi leo risus</li>
+                        <li class="list-group-item">Porta ac consectetur ac</li>
+                        <li class="list-group-item">Vestibulum at eros</li>
+                    </ul>
                     
-                    <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-                    	<div class="panel panel-default">
-                        	<div class="panel-heading" role="tab" id="headingOne">
-		                        <h4 class="panel-title">
-		                            <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        			                    Collapsible Group Item #1
-                    		        </a>
-							    </h4>
-						    </div>
-    						<div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
-      							<div class="panel-body">
-       								Description 1 
-							    </div>
-							</div>
-						</div>
-						<div class="panel panel-default">
-						<div class="panel-heading" role="tab" id="headingTwo">
-						    <h4 class="panel-title">
-        						<a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-							        Collapsible Group Item #2
-						        </a>
-					    	</h4>
-						</div>
-					    <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
-					    <div class="panel-body">
-					        Description 2
-      					</div>
-    				</div>
-  				</div>
-  				<div class="panel panel-default">
-    				<div class="panel-heading" role="tab" id="headingThree">
-      					<h4 class="panel-title">
-        					<a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-          						Collapsible Group Item #3
-        					</a>
-      					</h4>
-    				</div>
-    <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
-      <div class="panel-body">
-        Description 3
-      </div>
-    </div>
-  </div>
-</div>
 
-<script>$('.collapse').collapse()</script>
+                    <script>$('.collapse').collapse();</script>
                 </div>
             </div> 
         </div>
